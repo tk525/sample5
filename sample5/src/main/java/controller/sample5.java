ea10791737d6a2841f03feae3e28062b7f0703a6
滝口結 <[525tkg@gmail.com]>
2023-10-08 22:27:05 +0900

    初期構築〜ブラウザに文字表示

---
+++ sample5/src/main/java/controller/sample5.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class sample5
+ */
+@WebServlet("/sample5")
+public class sample5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("ABCD");
+	}
+
+}
